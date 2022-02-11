a5f024e07b5c0f5990873ddbf295e8d3bb94b7c7
Leia <87383466+klynmc@users.noreply.github.com>
2022-02-11 11:20:39 -0600

    add background img

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -5,7 +5,26 @@ import './home.css'
 const Home = () => {
     return (
         <div className="homepage">
-            <h1>Home Page</h1>
+            <div className="header">
+            <h1>Creature <br /> C🐾mforts</h1>
+            <h2>
+                There's no place like home
+            </h2>
+            <br />
+            </div>
+            <div className="about">
+            <h2>
+                Does your pet suffer from separation 
+                anxiety? Always come home from
+                the kennel sick and stressed? Do you
+                worry if they are given the proper love and 
+                attention that they need?
+                Creature Comforts was designed to provide
+                a better sense of security to both pets 
+                and owners by allowing your pet to stay
+                in the comfort of their own home while you're away.
+            </h2>
+            </div>
             <div className="sections">
                 <section className="section1">
                     <NavLink to="/about-me">
