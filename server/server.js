87d7b27f314df4581bf606741c9acf5ed6ea1439
Leia <87383466+klynmc@users.noreply.github.com>
2022-02-21 20:17:08 -0600

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+
+import path from 'path'
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors());
+app.use(express.json())
+
+const CONNECTION_URL = 'mongodb+srv://lmckay:lmckay123@cluster0.qzesj.mongodb.net/cc?retryWrites=true&w=majority'
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error));
