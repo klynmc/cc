7df761bbfc248a54d0ca35064ab898134baed299
Leia <87383466+klynmc@users.noreply.github.com>
2022-02-25 10:28:58 -0600

    fixed server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require ('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-import path from 'path'
-const __dirname = path.resolve();
+/* const path = require('path')
+const __dirname = path.resolve(); */
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-dotenv.config();
+/* dotenv.config(); */
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
