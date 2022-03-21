416d3dcb3e44647731162acdc05f81284aea66b4
Leia <87383466+klynmc@users.noreply.github.com>
2022-03-21 10:05:15 -0500

    install calendar package

--- client/src/components/Contact/Calendar/index.js
+++ client/src/components/Contact/Calendar/index.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './calendar.css'
+import reactCalendar from 'react-calendar'
 
 function Calendar() {
+
+    const [value, onChange] = useState(new Date());
 
     return (
         <div className="calendar-div">
                 <h2>Want to check availability?</h2>
                 <div className="calendar">
-                    <h3>*calendar here*</h3>
+                   {/*  <h3>*calendar here*</h3>  */}
+                    {/* {reactCalendar()} */}
                 </div>
         </div>
     )
